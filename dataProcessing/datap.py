f51ef99b1af2a7ec592cabd45791e967a4b45b93
Jason Jeon <nosarge@gmail.com>
2024-07-20 18:37:44 +1000

    separated out logic

--- dataProcessing/datap.py
+++ dataProcessing/datap.py
@@ -13,90 +13,97 @@ from CSIKit.util.filters import hampel
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_error
 
-my_reader = get_reader('./sample.pcap')
-csi_data = my_reader.read_file('./sample.pcap')
-csi_matrix, no_frames, no_subcarriers = csitools.get_CSI(csi_data, metric='amplitude')
 
-csi_matrix_first = csi_matrix [:, :, 0, 0]
-csi_matrix_squeezd = np.squeeze(csi_matrix_first)
-finite_mask = np.isfinite(csi_matrix_squeezd).all(axis=1)
-csi_matrix_squeezd = csi_matrix_squeezd[finite_mask]
+def get_pcap_df(pcap_file):
+    my_reader = get_reader(pcap_file)
+    csi_data = my_reader.read_file(pcap_file)
+    csi_matrix, no_frames, no_subcarriers = csitools.get_CSI(csi_data, metric='amplitude')
+    csi_matrix_first = csi_matrix [:, :, 0, 0]
+    csi_matrix_squeezd = np.squeeze(csi_matrix_first)
+    finite_mask = np.isfinite(csi_matrix_squeezd).all(axis=1)
+    csi_matrix_squeezd = csi_matrix_squeezd[finite_mask]
 
-for x in range(len(csi_matrix_squeezd)):
-    csi_matrix_squeezd[x] = lowpass(csi_matrix_squeezd[x], 10, 100, 5)
-    csi_matrix_squeezd[x] = hampel(csi_matrix_squeezd[x], 10, 3)
-    csi_matrix_squeezd[x] = running_mean(csi_matrix_squeezd[x], 10)
+    for x in range(len(csi_matrix_squeezd)):
+        csi_matrix_squeezd[x] = lowpass(csi_matrix_squeezd[x], 10, 100, 5)
+        csi_matrix_squeezd[x] = hampel(csi_matrix_squeezd[x], 10, 3)
+        csi_matrix_squeezd[x] = running_mean(csi_matrix_squeezd[x], 10)
 
-null_subcarriers = [-64, -63, -62, -61, -60, -59, -1, 0, 1, 59, 60, 61, 62, 63]
-pilot_subcarriers = [11, 25, 53, -11, -25, -53]
-removed_subcarriers = null_subcarriers + pilot_subcarriers
-removed_subcarriers.sort(reverse=True)
+    null_subcarriers = [-64, -63, -62, -61, -60, -59, -1, 0, 1, 59, 60, 61, 62, 63]
+    pilot_subcarriers = [11, 25, 53, -11, -25, -53]
+    removed_subcarriers = null_subcarriers + pilot_subcarriers
+    removed_subcarriers.sort(reverse=True)
 
-for i in removed_subcarriers:
-    csi_matrix_squeezd = np.delete(csi_matrix_squeezd, i + 64, 1)
+    for i in removed_subcarriers:
+        csi_matrix_squeezd = np.delete(csi_matrix_squeezd, i + 64, 1)
 
-# get the average value of the subcarriers
-df_csi = pd.DataFrame(csi_matrix_squeezd.mean(axis=1), columns=['csi'])
-columns = ['time', 'temp', 'rh']
-df_temp = pd.read_csv('./sample_temps.csv', header=None, names=columns)
+    # get the average value of the subcarriers
+    df_csi = pd.DataFrame(csi_matrix_squeezd.mean(axis=1), columns=['csi'])
+    return df_csi
 
-print(len(df_temp))
-interval_size = len(df_csi) / len(df_temp)
-print(interval_size)
+def get_temp_df(temp_file):
+    columns = ['time', 'temp']
+    df_temp = pd.read_csv(temp_file, header=None, names=columns)
+    print(len(df_temp))
 
 
-df_csi['group'] = df_csi.index // interval_size
-df_avg_csi = df_csi.groupby('group').mean().reset_index()
-if (len(df_avg_csi) > len(df_temp)):
-    df_avg_csi = df_avg_csi.head(len(df_temp))
-else:
-    df_temp = df_temp.head(len(df_avg_csi))
+def combine_average_df(df_csi, df_temp):
+    interval_size = len(df_csi) / len(df_temp)
+    df_csi['group'] = df_csi.index // interval_size
+    df_avg_csi = df_csi.groupby('group').mean().reset_index()
+    if (len(df_avg_csi) > len(df_temp)):
+        df_avg_csi = df_avg_csi.head(len(df_temp))
+    else:
+        df_temp = df_temp.head(len(df_avg_csi))
+    df_combined = pd.concat([df_temp.reset_index(drop=True), df_avg_csi], axis=1)
+    return df_combined
 
-df_combined = pd.concat([df_temp.reset_index(drop=True), df_avg_csi], axis=1)
+def train_ml(df_combined):
+    # # # now the machine learning
+    x_rf = df_combined[['temp']]
+    y_rf = df_combined['csi']
+    model = RandomForestRegressor()
+    # model = LinearRegression()
+    x_train, x_test, y_train, y_test = train_test_split(x_rf, y_rf, test_size=0.5, random_state=42)
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    # kfold training
+    k = 5
+    kf = KFold(n_splits=5, shuffle=True, random_state=0)
 
-# # # now the machine learning
-x_rf = df_combined[['temp']]
-y_rf = df_combined['csi']
+    # Perform K-Fold Cross-Validation
+    mse_list = []
 
-model = RandomForestRegressor()
-# model = LinearRegression()
-x_train, x_test, y_train, y_test = train_test_split(x_rf, y_rf, test_size=0.5, random_state=42)
-model.fit(x_train, y_train)
-y_pred = model.predict(x_test)
+    for train_index, test_index in kf.split(df_combined):
+        # Split data
+        X_train, X_test = df_combined[['temp']].iloc[train_index], df_combined[['temp']].iloc[test_index]
+        y_train, y_test = df_combined['csi'].iloc[train_index], df_combined['csi'].iloc[test_index]
 
-# kfold training
-k = 5
-kf = KFold(n_splits=5, shuffle=True, random_state=0)
+        # Train the model
+        model.fit(X_train, y_train)
 
-# Perform K-Fold Cross-Validation
-mse_list = []
+        # Make predictions
+        y_pred = model.predict(X_test)
 
-for train_index, test_index in kf.split(df_combined):
-    # Split data
-    X_train, X_test = df_combined[['temp']].iloc[train_index], df_combined[['temp']].iloc[test_index]
-    y_train, y_test = df_combined['csi'].iloc[train_index], df_combined['csi'].iloc[test_index]
+        # Evaluate the model
+        mse = mean_squared_error(y_test, y_pred)
+        mse_list.append(mse)
 
-    # Train the model
-    model.fit(X_train, y_train)
+def graph_ml(df_combined, x_test, y_pred, model):
+    # machine learning graphing
+    plt.scatter(df_combined['temp'], df_combined['csi'], color='blue')
+    y_pred = model.predict(x_test)
+    plt.scatter(x_test, y_pred, color='red')
+    plt.legend()
+    plt.show()
 
-    # Make predictions
-    y_pred = model.predict(X_test)
+def get_regression(df_combined):
+    # polynomial
+    model = np.poly1d(np.polyfit(df_combined['csi'], df_combined['temp'], 3))
+    polyline = np.linspace(min(df_combined['csi']), max(df_combined['csi']), 1000)
+    print(polyline)
+    plt.scatter(df_combined['csi'], df_combined['temp'])
+    plt.plot(polyline, model(polyline))
+    plt.show()
 
-    # Evaluate the model
-    mse = mean_squared_error(y_test, y_pred)
-    mse_list.append(mse)
-
-# machine learning graphing
-plt.scatter(df_combined['temp'], df_combined['csi'], color='blue')
-y_pred = model.predict(x_test)
-plt.scatter(x_test, y_pred, color='red')
-plt.legend()
-plt.show()
-
-# polynomial
-# model = np.poly1d(np.polyfit(df_combined['csi'], df_combined['temp'], 3))
-# polyline = np.linspace(min(df_combined['csi']), max(df_combined['csi']), 1000)
-# print(polyline)
-# plt.scatter(df_combined['csi'], df_combined['temp'])
-# plt.plot(polyline, model(polyline))
-# plt.show()
+if __name__ == "__main__":
+    print("hi")
