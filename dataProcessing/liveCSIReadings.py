359815991d686f5b0ea0eac98efd3c129fdceb80
ethanm20 <z5312704@ad.unsw.edu.au>
2024-08-03 17:11:43 +1000

    CSI3

--- dataProcessing/liveCSIReadings.py
+++ dataProcessing/liveCSIReadings.py
@@ -5,20 +5,6 @@ import AWSIoTPythonSDK.MQTTLib as AWSIoTpyMQTT
 # AWS MQTT CONFIGURATION
 #-------------------------------------------------------------------
 
-def new_mqtt_message(client, userdata, message):
-    msgPayload = message.payload
-    msgPayloadStr = msgPayload.decode('utf-8')
-
-    print('MQTT RECEIVED: MESSAGE PAYLOAD')
-    print(msgPayloadStr)
-
-    messageObj = json.loads(msgPayloadStr)
-
-    if (messageObj['fire_alarm'] == 1):
-        fire_alarm(messageObj['temperature_predicted'])
-    elif (messageObj['temperature_predicted'] != -1):
-        display_predicted_temperature(messageObj['temperature_predicted'])
-
 # Client configuration with endpoint and credentials
 myClient = AWSIoTpyMQTT.AWSIoTMQTTClient("iotconsole-ffb21e69-bfb3-46d4-b53e-770684c37161")
 myClient.configureEndpoint('a9p2nsgtl0l6h-ats.iot.ap-southeast-2.amazonaws.com',8883)
@@ -27,9 +13,23 @@ myClient.configureConnectDisconnectTimeout(10)
 
 myClient.connect()
 
+def send_mqtt_message(predicted_temp = -1, fire_alarm = -1):
+    # fire_alarm Should be 1 for ON
+    # fire_alarm should be either 0 or -1 for OFF
+    # predicted_temp should be number of the predicted temperature in Celcius
+    payload = {
+        "temperature_predicted": predicted_temp,
+        "fire_alarm": fire_alarm
+    }
+
+    myClient.publish("test/comp6733", json.dumps(payload), 0)
+
+#TODO: EXAMPLE 30 Degrees, Fire Alarm On
+send_mqtt_messsage(30, 1)
+
+
+
 myClient.subscribe("test/comp6733", 0, new_mqtt_message)
 
-while True:
-    time.sleep(5)
 
 
